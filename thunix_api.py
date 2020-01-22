860f78f20679023d857f1347866d3cf7505f4717
Ubergeek <ubergeek@yourtilde.com>
2020-01-22 14:28:44 -0500

    More fleshing it out

--- thunix_api.py
+++ thunix_api.py
@@ -2,7 +2,12 @@
 import flask
 
 from flask import Flask, request, jsonify
+
+import psutil, datetime
+
+
 app = Flask(__name__)
+
 
 # No endpoint selected
 @app.route("/")
@@ -10,31 +15,37 @@ def home():
     return "The Thunix API.  Please see https://wiki.thunix.net/wiki/api for more information."
     app.run()
 
+
 # ip_info
 @app.route("/ip_info")
 def ip_info():
     return "IP Info"
     app.run()
 
+
 # uptime
 @app.route("/uptime")
 def uptime():
-    return "Uptime"
+
+    return str(datetime.timedelta(seconds=psutil.boot_time()))
     app.run()
+
 
 # teapot
 @app.route("/teapot")
 def teapot():
-    teapot = [
-{
-   "tea" : "available",
-   "height" : "short",
-   "width" : "stout"
-}
-]
-    return jsonify(teapot)
-    app.run() 
+    teapots = [
+        {
+            "tea": "available",
+            "height": "short",
+            "width": "stout"
+        }
+    ]
+    return jsonify(teapots)
+    app.run()
+
 
 # main loop
-if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+if __name__ == "__main__":  # on running python app.py
+    app.run()  # run the flask app
+
