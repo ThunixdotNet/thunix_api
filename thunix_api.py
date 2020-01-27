f43043efe2f37707d957ec98ff090fd65f3b64a9
Ubergeek <ubergeek@yourtilde.com>
2020-01-27 11:01:35 -0500

    Matching urls to spec

--- thunix_api.py
+++ thunix_api.py
@@ -9,14 +9,15 @@ from endpoints import disk, home, ip_info, load, memory, teapot, uptime
 
 app = Flask(__name__) # pylint: disable=C0103
 api = Api(app) # pylint: disable=C0103
-
-api.add_resource(disk.Disk, "/disk")
-api.add_resource(home.Home, "/")
-api.add_resource(ip_info.IpInfo, "/ip_info")
-api.add_resource(load.Load, "/load")
-api.add_resource(memory.Memory, "/mem")
-api.add_resource(teapot.Teapot, "/teapot")
-api.add_resource(uptime.Uptime, "/uptime")
+api_version = "1"
+url_prepend = "/api/" + api_version
+api.add_resource(disk.Disk, url_prepend +  "/disk")
+api.add_resource(home.Home, url_prepend )
+api.add_resource(ip_info.IpInfo, url_prepend + "/ip_info")
+api.add_resource(load.Load, url_prepend + "/load")
+api.add_resource(memory.Memory, url_prepend + "/mem")
+api.add_resource(teapot.Teapot, url_prepend + "/teapot")
+api.add_resource(uptime.Uptime, url_prepend + "/uptime")
 
 if __name__ == "__main__":
     app.run()
